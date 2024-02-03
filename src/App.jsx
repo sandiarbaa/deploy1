fd3b35809d1865d783ee19af019c1cdbb4f3ef0e
sandiarbaa <sandiarbaa@gmail.com>
2024-02-03 15:54:29 +0700

    add: welcome vite card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import "./App.css";
-
 function App() {
   return (
-    <div>
-      <h2>Deploy 1</h2>
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="w-96 p-5 border shadow rounded-md bg-white hover:bg-sky-500 hover:text-white duration-300">
+        <h1 className="text-2xl font-bold text-center mb-3">Vite + React</h1>
+        <p className="text-sm">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora sint
+          obcaecati ipsam eos. Corporis dolore fuga beatae perferendis
+          molestias. Magni reprehenderit molestias dolorum at perspiciatis.
+          Dicta, eum sit. Sed, accusamus!
+        </p>
+      </div>
     </div>
   );
 }
